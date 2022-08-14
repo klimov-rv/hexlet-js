08e348640cf8c1917a8e7ea1f3e6bd0f4fab62f3
Klimov Roman <klimov.rv@ya.ru>
2022-08-14 13:22:27 +0300

    eslint done

--- hexlet-hello-world/index.js
+++ hexlet-hello-world/index.js
@@ -1,12 +1,11 @@
-console.log("Hello, Hexlet!");
-
 // По общепринятому соглашению, lodash импортируется под именем _
-import _ from "lodash";
+import _ from 'lodash';
 
 // Пересечение массивов
 _.intersection([2, 1], [2, 3]); // => [2]
 
 // capitalize делает первую букву заглавной
-_.capitalize("hello"); // Hello
+_.capitalize('hello'); // Hello
 
-console.log(_.last(["one", "two"]));
+// console.log('Hello, Hexlet!');
+// console.log(_.last(['one', 'two']));
