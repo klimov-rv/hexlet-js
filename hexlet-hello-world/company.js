f9f9a3f921598918ae9ba73e132543e82fe3d13e
Klimov Roman <klimov.rv@ya.ru>
2022-08-14 15:40:55 +0300

    step 4 - objects

---
+++ hexlet-hello-world/company.js
@@ -0,0 +1,8 @@
+export default function is(comp1, comp2) {
+    if (comp1 === comp2) {
+        let eql = true;
+    } else {
+        let eql = false;
+    }
+    return eql;
+}
--- hexlet-hello-world/index.js
+++ hexlet-hello-world/index.js
@@ -1,6 +1,8 @@
 // По общепринятому соглашению, lodash импортируется под именем _
 import _ from 'lodash';
 
+import is from './company.js';
+
 // Пересечение массивов
 _.intersection([2, 1], [2, 3]); // => [2]
 
@@ -9,3 +11,27 @@ _.capitalize('hello'); // Hello
 
 // console.log('Hello, Hexlet!');
 // console.log(_.last(['one', 'two']));
+
+const user = {
+  name: 'roman',
+  married: true,
+  age: 34,
+};
+
+_.capitalize(user.name);
+
+
+
+ 
+
+const company1 = { name: 'Hexlet', state: 'moderating', website: 'https://hexlet.io' };
+
+const company2 = { name: 'CodeBasics', state: 'published', website: 'https://code-basics.com' };
+
+is(company1, company2); // false
+
+// const company1 = { name: 'Hexlet', state: 'published', website: 'https://hexlet.io' };
+
+// const company2 = { name: 'Hexlet', state: 'published', website: 'https://hexlet.io' };
+
+// is(company1, company2); // true
