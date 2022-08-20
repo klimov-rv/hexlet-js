ef193d6bf568359b1be2be061833bc4075c2dda0
Klimov Roman <klimov.rv@ya.ru>
2022-08-20 13:17:22 +0300

    fix eslint

--- hexlet-hello-world/objects.js
+++ hexlet-hello-world/objects.js
@@ -1,3 +1,5 @@
 // import _ from 'lodash';
 
-export default (nameForObj, paramsForObj) => ({ ...{ name: nameForObj }, state: 'moderating', createdAt: Date(), ...paramsForObj });
+export default (nameForObj, paramsForObj) => ({
+  ...{ name: nameForObj }, state: 'moderating', createdAt: Date(), ...paramsForObj,
+});
