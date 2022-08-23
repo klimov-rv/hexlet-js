1a0ea8bd5b0c75f77bb103d99431beedaa9d7bc7
Klimov Roman <klimov.rv@ya.ru>
2022-08-23 17:51:07 +0300

    scrabble.js - init

--- hexlet-hello-world/index.js
+++ hexlet-hello-world/index.js
@@ -1,12 +1,7 @@
-import { make, set, get } from './map.js';
+import scrabble from './scrabble.js';
  
-const map = make();
-let result = get(map, 'key');
-console.log(result); // => null
- 
-result = get(map, 'key', 'default_value');
-console.log(result); // => "default_value"
- 
-set(map, 'key2', 'value2');
-result = get(map, 'key2');
-console.log(result); // => "value2"
+scrabble('rkqodlw', 'world'); // true
+scrabble('avj', 'java'); // false
+scrabble('avjafff', 'java'); // true
+scrabble('', 'hexlet'); // false
+scrabble('scriptingjava', 'JavaScript'); // true
